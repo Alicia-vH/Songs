7a7e535803c86654666d15fb5e89858536c18bd9
Alicia-vH <127492012+Alicia-vH@users.noreply.github.com>
2023-09-26 14:03:04 +0200

    seeder succeeded, songs added to db

--- database/seeders/SongsTableSeeder.php
+++ database/seeders/SongsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class SongsTableSeeder extends Seeder
 {
@@ -12,6 +14,34 @@ class SongsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('songs')->insert([
+            'title' => 'Come Take My Hand',
+            'singer' => '2 Brothers on The 4th Floor',
+            'created_at' => now(),
+        ]);
+
+        DB::table('songs')->insert([
+            'title' => 'Snap Out Of It',
+            'singer' => 'Artic Monkeys',
+            'created_at' => now(),
+        ]);
+
+        DB::table('songs')->insert([
+            'title' => 'My Way',
+            'singer' => 'Elvis Presley',
+            'created_at' => now(),
+        ]);
+
+        DB::table('songs')->insert([
+            'title' => 'Guilty Conscience',
+            'singer' => 'Eminem, Dr. Dre',
+            'created_at' => now(),
+        ]);
+
+        DB::table('songs')->insert([
+            'title' => 'Tattoo',
+            'singer' => 'Loreen',
+            'created_at' => now(),
+        ]);
     }
 }
